65274f320488f9bf232fbac73eaa2dd21303b63a
DavidBalishyan <davidbalishyan12@gmail.com>
2026-04-27 15:11:58 +0400

    Update

--- reverse_string.c
+++ reverse_string.c
@@ -1,8 +1,9 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include "clibx.h"
 
 /*
- * String reversal (in-place)
+ * String reversal
  * --------------------------------------
  * Given a null-terminated string, reverse its characters in place.
  * The function swaps characters from the beginning and end,
@@ -13,23 +14,26 @@
  * Average performance O(n)
  * Worst-case space complexity O(1) iterative
  * */
-void reverseString(char str[]) {
-    int length = strlen(str);
+str reverseString(char string[]) {
+    str copy = malloc(strlen(string) + 1);
+    strcpy(copy, string);
+    int length = strlen(copy);
     int start = 0, end = length - 1;
     char temp;
 
     while (start < end) {
-        temp = str[start];
-        str[start] = str[end];
-        str[end] = temp;
+        temp = copy[start];
+        copy[start] = copy[end];
+        copy[end] = temp;
         start++;
         end--;
     }
+    return copy;
 }
 
 int main() {
     char str[] = "Hello";
-    reverseString(str);
-    printf("Reversed string: %s\n", str);
+    PRINT(reverseString(str));
+    LOG("%s\n", str);
     return 0;
 }
